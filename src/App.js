c9c3083195a5ef99b93fa0274191d604250130cc
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-03 18:38:09 -0400

    combain all components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import List from "./list"
+import Search from "./search"
+import Main from "./main"
+import Otherdays from "./otherdays"
+import Git from "./git"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List />
+      <Search />
+      <Main />
+      <Otherdays />
+      <Git />
     </div>
   );
 }
