69f444b1779ee31c7ba64a375a33e2bc220391c1
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-03 18:36:59 -0400

    add structure for other days

---
+++ src/otherdays.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+export default function Otherdays(){
+    return(<div>
+        <ul className="flex-container">
+        <li><div className="other-days auto"> 
+            <div className="day">123</div>
+            <img/>
+            <div className="degree">312</div>
+            </div></li>
+        <li><div className="other-days auto"> 
+            <div className="day">123</div>
+            <img/>
+            <div className="degree">312</div>
+            </div></li>
+        <li><div className="other-days auto"> 
+            <div className="day">123</div>
+            <img/>
+            <div className="degree">312</div>
+            </div></li>
+        <li><div className="other-days auto"> 
+            <div className="day">123</div>
+            <img/>
+            <div className="degree">312</div>
+            </div></li>
+        <li><div className="other-days auto">
+            <div className="day">123</div>
+            <img/>
+            <div className="degree">312</div>
+            </div></li>
+        </ul>
+    </div>)
+}
