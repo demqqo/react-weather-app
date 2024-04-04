06e9d6d6f3fac352a03c0e25491abd8a00aabfc3
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-04 18:21:29 -0400

    add event listener for form

--- src/search.js
+++ src/search.js
@@ -1,13 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 import "./App.css"
-export default function Search(){
+import Main from "./main"
+export default function Search(props){
+    let [city, setCity] = useState(` `) 
+    let [city1, setCity1] = useState(` `)
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        
+        setCity(city1)
+        
+console.log(city)
+    }
     return(
-        <div >
-            <ul className="flex-container">
-        <li><input className="search-box initial" type="text" placeholder="Enter a city"/></li>
-
-        <li><input className="submit-button none" type="button" value="Search"/></li>
-        <li><input className="current-button none" type="button" value="Current"/></li>
-        </ul></div>
+        <div className="flex-container" >
+            <form onSubmit={handleSubmit}>
+                <input className="search-box initial" type="text" placeholder="Enter a city" onChange = {(e)=>setCity1(e.target.value)}/>
+            <input className="submit-button none" type="submit" value="Search" onSubmit={handleSubmit}/>
+            <input className="current-button none" type="button" value="Current"/></form>
+       <Main city={city}/> </div>
     )
 }
