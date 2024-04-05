d5e666deecacf191cff0853d14a122ac18d6980a
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-04 20:05:13 -0400

    add event listener to list and tried to bite it with search component
    doesn't work yet

--- src/list.js
+++ src/list.js
@@ -1,13 +1,22 @@
-import React from "react" 
+import React , {useState}from "react" 
 import "./App.css"
+import Search from "./search"
+
 export default function List(){
+    let [city,setCity] = useState(` `)
+    function handleSubmit(e){
+e.preventDefault();
+setCity(e)
+console.log(city)
+    }
     return(
-        <div>
-        <ul className="flex-container">
-            <li className="item initial"><a href="#">Lisbon</a></li>
-            <li className="item none"><a href="#">Paris</a></li>
-            <li className="item none"><a href="#">Sydney</a></li>
-            <li className="item none"><a href="#">San Francisco</a></li>
-        </ul></div>
+        <div className="flex-container list" >
+        <form onSubmit={ handleSubmit}>
+            <input className="item initial" type="submit" value="Lisbon" onClick = { (e) => setCity(e.target.value)} />
+            <input className="item none" type="submit" value="Paris"onClick = { (e) => setCity(e.target.value)} />
+            <input className="item none" type="submit" value="Sydney"onClick = { (e) => setCity(e.target.value)} />
+            <input className="item none" type="submit"value="San Francisco"onClick = { (e) => setCity(e.target.value)} /></form>
+            {/*<Search city={city}/>*/}
+        </div>
     )
 }
