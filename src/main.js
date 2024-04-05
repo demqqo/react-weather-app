15c80215329121b9ae2a543ad8ead8d8b7a39954
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-04 20:06:11 -0400

    start to show real data from weather api
    in plan finish: -Date -Wind -visibility -icon -precipitation

--- src/main.js
+++ src/main.js
@@ -1,17 +1,36 @@
-import React from "react"
+import React, {useState} from "react"
+import axios from "axios"
 
-export default function Main(){
+export default function Main(props){
+    const[city,setCity] =useState(" ")
+    const [temperature, setTemperature] = useState(" ")
+    const [humidity, setHumidity] = useState(" ")
+    const [precipitation, setPrecipitation] = useState(" ")
+    const [date, setDate] = useState(" ")
+    const [imgurl, setImgurl] = useState(" ");
+    let key = `baf054037a66413c8db15210241703`
+    
+let URL = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${props.city}`
+axios.get(`${URL}`).then(function (response){
+    setImgurl(response.data.current.condition.icon);
+    setPrecipitation(`precipitation: ${Math.round(response.data.current.precip_mm)} mm`);
+    setHumidity(`Humidity: ${response.data.current.humidity}%`);
+    setTemperature(`${Math.round(response.data.current.temp_c)}°C`);
+    setDate(response.data.location.localtime_epoch)
+    console.log(Date(date))
+    
+})
     return(
         <div>
             <div className="flex-container">
-                <ul><li><h1>Lisbon</h1></li>
+                <ul><li><h1>{props.city} {temperature}</h1></li>
                 <li>Wednesday 14:26</li>
                 <li>Clear</li></ul>
             </div>
             <div className="flex-container">
                 <img className="main-degree" alt="degree"/>
                 <ul>
-                <li>Precipitation: 86%</li><li>Wind: 4km/h</li>
+                <li>Precipitation: {precipitation}</li><li>Wind: 4km/h</li>
 
                 </ul>
 
