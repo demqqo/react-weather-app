bd95e299c058c545efc68394fc73c673b08f9879
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-06 18:33:57 -0400

    add all data from main
    logo/wind/visibility/day/hour/temperature/city/precipitation/City(so if user try to find city in lower case it will show him city in uppercase)

--- src/main.js
+++ src/main.js
@@ -5,33 +5,43 @@ import moment from 'moment';
 export default function Main(props){
     const[city,setCity] =useState(" ")
     const [temperature, setTemperature] = useState(" ")
-    const [humidity, setHumidity] = useState(" ")
+    const [condition, setCondition] = useState(" ")
     const [precipitation, setPrecipitation] = useState(" ")
     const [date, setDate] = useState(" ")
     const [imgurl, setImgurl] = useState(" ");
+    const [wind, setWind] = useState(" ")
+    const [hour, setHour] = useState(" ")
+    const [day, setDay] = useState(" ")
     let key = `baf054037a66413c8db15210241703`
     
 let URL = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${props.city}`
+console.log(URL)
 axios.get(`${URL}`).then(function (response){
     setImgurl(response.data.current.condition.icon);
-    setPrecipitation(`precipitation: ${Math.round(response.data.current.precip_mm)} mm`);
-    setHumidity(`Humidity: ${response.data.current.humidity}%`);
+    setPrecipitation(`${Math.round(response.data.current.precip_mm)} mm`);
+    setWind(`${Math.round(response.data.current.wind_kph)}`)
     setTemperature(`${Math.round(response.data.current.temp_c)}°C`);
-    setDate(response.data.location.localtime_epoch)
-    console.log(Date(date))
-    
+    setCondition(response.data.current.condition.text)
+    console.log(response.data)
+   setDate(response.data.location.localtime)
+   setCity(response.data.location.name)
+   var day = moment("2024-04-06").day(); // for the number
+   day = moment("2024-04-06").format('dddd'); // for the word
+   setDay(day)
+   let example = response.data.location.localtime;
+setHour(example.substring(example.lastIndexOf(" ") + 1));
 })
     return(
         <div>
             <div className="flex-container">
-                <ul><li><h1>{props.city} {temperature}</h1></li>
-                <li>Wednesday 14:26</li>
-                <li>Clear</li></ul>
+                <ul><li><h1>{city} {temperature}</h1></li>
+                <li>{hour} {day}</li>
+                <li>{condition}</li></ul>
             </div>
             <div className="flex-container">
-                <img className="main-degree" alt="degree"/>
+                <img className="main-degree" alt="degree" src={imgurl}/>
                 <ul>
-                <li>Precipitation: {precipitation}</li><li>Wind: 4km/h</li>
+                <li>Precipitation: {precipitation}</li><li>Wind: {wind}km/h</li>
 
                 </ul>
 
