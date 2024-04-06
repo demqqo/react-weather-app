8d8466caf4d87ef9fa2441ef3b2d2ea13688fb75
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-06 18:31:21 -0400

    made a more understandable temporary other days instead of 123

--- src/otherdays.js
+++ src/otherdays.js
@@ -4,29 +4,29 @@ export default function Otherdays(){
     return(<div>
         <ul className="flex-container">
         <li><div className="other-days auto"> 
-            <div className="day">123</div>
+            <div className="day">Mon</div>
             <img/>
-            <div className="degree">312</div>
+            <div className="degree">31°C</div>
             </div></li>
         <li><div className="other-days auto"> 
-            <div className="day">123</div>
+            <div className="day">Tue</div>
             <img/>
-            <div className="degree">312</div>
+            <div className="degree">3°C</div>
             </div></li>
         <li><div className="other-days auto"> 
-            <div className="day">123</div>
+            <div className="day">Wed</div>
             <img/>
-            <div className="degree">312</div>
+            <div className="degree">3°C</div>
             </div></li>
         <li><div className="other-days auto"> 
-            <div className="day">123</div>
+            <div className="day">Thu</div>
             <img/>
-            <div className="degree">312</div>
+            <div className="degree">3°C</div>
             </div></li>
         <li><div className="other-days auto">
-            <div className="day">123</div>
+            <div className="day">Fri</div>
             <img/>
-            <div className="degree">312</div>
+            <div className="degree">3°C</div>
             </div></li>
         </ul>
     </div>)
