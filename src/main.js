e09c4ce5095a646e06623cfee993b19239853e02
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-06 23:16:34 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import React, {useState} from "react"
 import axios from "axios"
 import moment from 'moment';
-
+import Otherdays from "./otherdays"
 export default function Main(props){
-    const[city,setCity] =useState(" ")
+    const[city,setCity] =useState("Lisbon")
     const [temperature, setTemperature] = useState(" ")
     const [condition, setCondition] = useState(" ")
     const [precipitation, setPrecipitation] = useState(" ")
@@ -15,14 +15,12 @@ export default function Main(props){
     let key = `baf054037a66413c8db15210241703`
     
 let URL = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${props.city}`
-console.log(URL)
 axios.get(`${URL}`).then(function (response){
     setImgurl(response.data.current.condition.icon);
     setPrecipitation(`${Math.round(response.data.current.precip_mm)} mm`);
     setWind(`${Math.round(response.data.current.wind_kph)}`)
     setTemperature(`${Math.round(response.data.current.temp_c)}°C`);
     setCondition(response.data.current.condition.text)
-    console.log(response.data)
    setDate(response.data.location.localtime)
    setCity(response.data.location.name)
    var day = moment("2024-04-06").day(); // for the number
@@ -33,6 +31,7 @@ setHour(example.substring(example.lastIndexOf(" ") + 1));
 })
     return(
         <div>
+            
             <div className="flex-container">
                 <ul><li><h1>{city} {temperature}</h1></li>
                 <li>{hour} {day}</li>
