e1bbb2c751e6b654d259eb32a3720f762ae8ee94
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-06 23:16:21 -0400

    Update list.js

--- src/list.js
+++ src/list.js
@@ -1,22 +1,25 @@
 import React , {useState}from "react" 
 import "./App.css"
 import Search from "./search"
+import Main from "./main"
 
-export default function List(){
+export default function List(props){
     let [city,setCity] = useState(` `)
-    function handleSubmit(e){
+    let [city1,setCity1] = useState("Sydney")
+    const handleSubmit=(e)=>{
 e.preventDefault();
-setCity(e)
+setCity1(city)
 console.log(city)
     }
+    
     return(
+        <div>
         <div className="flex-container list" >
         <form onSubmit={ handleSubmit}>
             <input className="item initial" type="submit" value="Lisbon" onClick = { (e) => setCity(e.target.value)} />
             <input className="item none" type="submit" value="Paris"onClick = { (e) => setCity(e.target.value)} />
             <input className="item none" type="submit" value="Sydney"onClick = { (e) => setCity(e.target.value)} />
             <input className="item none" type="submit"value="San Francisco"onClick = { (e) => setCity(e.target.value)} /></form>
-            {/*<Search city={city}/>*/}
             
             </div><Search city={city1} key={city1}/>
         </div>
