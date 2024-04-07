d798c321b86dbc87b9340496bc4883e7f5cb5c5d
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-06 23:18:35 -0400

    add functional for button curent and google api
    google api helps determine city name with lat and lon

--- src/search.js
+++ src/search.js
@@ -1,8 +1,18 @@
 import React, {useState} from "react"
 import "./App.css"
 import Main from "./main"
+import axios from "axios"
 export default function Search(props){
+    
+    const [lat, setLat] = useState(65)
+    const [lon, setLon] = useState(45)
+    const [city2, setCity2] = useState(" toronto ")
 
+  let key = "AIzaSyA6AGrPnASatY4WeXcV84lG2k9eycEBYt4"
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&sensor=true&key=${key}`
+  
+  
+  
     let [city, setCity] = useState(props.city) 
     let [city1, setCity1] = useState(`Bali`)
     
@@ -10,15 +20,37 @@ export default function Search(props){
     const handleSubmit=(e)=>{
         e.preventDefault();
         
+    const handleCurent=(e)=>{
+        e.preventDefault();
         
-        setCity(city1)
+        
+        getLocation()
+
+function getLocation() {
+    function showPosition(position) {
+    setLat(position.coords.latitude) 
+    setLon(position.coords.longitude)
+    
+    axios.get(url).then(function(response){console.log(1)
+setCity2(response.data.plus_code.compound_code);
+if(city2){
+setCity((city2).split(" ")[1])}})
+  }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+     alert("Geolocation is not supported by this browser.");
     }
+  }
+
+    }
+    
     return(
         <div className="flex-container" >
             <form onSubmit={handleSubmit}>
                 <input className="search-box initial" type="text" placeholder="Enter a city"  onChange = {(e)=>setCity1(e.target.value)  }/>
             <input className="submit-button none" type="submit" value="Search" onSubmit={handleSubmit}/>
-            <input className="current-button none" type="button" value="Current"/></form>
+            </form><button className="current-button none" type="button" value="Current" onClick={handleCurent}>Current</button>
         <Main city={city}/></div>
       
     )
